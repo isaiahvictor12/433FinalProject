08884c1a7420b04d84e62d8c61b7a5f29928a5d6
Willow Ciesialka <wciesialka@gmail.com>
2023-04-30 23:17:46 -0400

    added routing

--- src/sensornetwork/web/__init__.py
+++ src/sensornetwork/web/__init__.py
@@ -18,5 +18,25 @@ connection = mysql.connector.connect(
 # Home Page25
 @route('/')
 def index():
+    data = {}
+    with connection.cursor() as cursor:
+        temperature_q = '''SELECT data_record.recording "Y", data_record.record_time "X" 
+                           FROM temperature_recording JOIN data_record
+                           ON data_record.record_id = temperature_recording.record_id
+                           ORDER BY data_record.record_time DESC;'''
+        execution_result = cursor.execute(temperature_q)
+        data['temperature'] = cursor.fetch_all()
+        humidity_q = '''SELECT data_record.recording "Y", data_record.record_time "X" 
+                        FROM humidity_recording JOIN data_record
+                        ON data_record.record_id = humidity_recording.record_id
+                        ORDER BY data_record.record_time DESC;'''
+        execution_result = cursor.execute(humidity_q)
+        data['humidity'] = cursor.fetch_all()
+        light_q = '''SELECT data_record.recording "Y", data_record.record_time "X" 
+                     FROM light_recording JOIN data_record
+                     ON data_record.record_id = light_recording.record_id
+                     ORDER BY data_record.record_time DESC;'''
+        execution_result = cursor.execute(light_q)
+        data['light'] = cursor.fetch_all()
     file = TEMPLATES['index.tpl']
-    return template(file, (None, ))
+    return template(file, data=data)
