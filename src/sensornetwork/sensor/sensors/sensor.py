8d25ab15582c5c7da07bc3cdeb10177bae1007ef
Willow Ciesialka <wciesialka@gmail.com>
2023-05-02 23:25:48 -0400

    aaaaaaa3

--- src/sensornetwork/sensor/sensors/sensor.py
+++ src/sensornetwork/sensor/sensors/sensor.py
@@ -11,19 +11,22 @@ class Sensor(ABC):
         self._collecting = True
         self._cache_lock = threading.Lock()
         self._thread = threading.Thread(target=self._thread_target)
-        self._thread.start()
+        self._started = False
 
     @abstractmethod
     def _read_data(self) -> float:
         pass
 
     def _thread_target(self):
+        self._started = True
         while self._collecting:
             with self._cache_lock:
                 self._cached_reading = self._read_data()
             sleep(self.polling_time)
 
     def get_reading(self):
+        if not self._started:
+            self._thread.start()
         while self._cached_reading is None:
             pass
         return self._cached_reading
